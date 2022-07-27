e046d673e01c970e7b52d1c1ca7be0d93f398b5c
DarkWolf162004 <ariasburiticajose2055@gmail.com>
2022-07-27 11:51:53 -0500

    se duplico y modifico el cuadro

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-const frame = document.getElementById('frame');
+var original = document.getElementById("frame");
+var clone = original.cloneNode(true);
+clone.removeAttribute("id");
+
+clone.getElementsByClassName("posicion-name")[0].innerHTML="Fulstack Diveloped"
+clone.getElementsByClassName("images")[0].src="images/manage.svg"
 
 
 
-frame.after(frame.cloneNode(true))
 
-frame.after(frame.cloneNode(true))
 
-frame.after(frame.cloneNode(true))
+original.after(clone)
 
-frame.after(frame.cloneNode(true))
 
-frame.after(frame.cloneNode(true))
-
-frame.after(frame.cloneNode(true))
-
-frame.after(frame.cloneNode(true))
-
-frame.after(frame.cloneNode(true))
-
-frame.after(frame.cloneNode(true))
 
 
 
