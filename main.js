f1a7cd63ffa5502afaf9d41cf27e358c965bf952
altovoy-ayte <alejandro.torres@itconsultores.com.co>
2022-08-02 10:30:34 -0500

    FEATURE/CLONE_JOB_ITEM introduce clone job item into a function to reuse that

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
-var original = document.getElementById("frame");
-var clone = original.cloneNode(true);
-clone.removeAttribute("id");
+const original = document.getElementById("frame");
 
+function createJobItem() {
+  const clone = original.cloneNode(true);
+  clone.removeAttribute("id");
+  clone.getElementsByClassName("posicion-name")[0].innerHTML =
+    "Fullstack Developer";
+  clone.getElementsByClassName("images")[0].src = "images/manage.svg";
+  clone.getElementsByClassName("enterprise-name")[0].innerHTML = "Manage";
+  clone.getElementsByClassName("agreement")[0].innerHTML = "Part Time";
+  clone.getElementsByClassName("country")[0].innerHTML = "Remote";
+  clone.getElementsByClassName("role")[0].innerHTML = "Fullstack";
+  clone.getElementsByClassName("level")[0].innerHTML = "Midweight";
+  clone.getElementsByClassName("web-page")[0].innerHTML = "Python";
+  clone.getElementsByClassName("blade-style")[0].innerHTML = "React";
+  clone.getElementsByClassName("programming")[0].style.display = "none";
+  clone.classList.remove("border-left-green");
+  original.after(clone);
+}
 
-
-clone.getElementsByClassName("posicion-name")[0].innerHTML="Fullstack Developer"
-clone.getElementsByClassName("images")[0].src="images/manage.svg"
-clone.getElementsByClassName("enterprise-name")[0].innerHTML="Manage"
-clone.getElementsByClassName("agreement")[0].innerHTML="Part Time"
-clone.getElementsByClassName("country")[0].innerHTML="Remote"
-clone.getElementsByClassName("role")[0].innerHTML="Fullstack"
-clone.getElementsByClassName("level")[0].innerHTML="Midweight"
-clone.getElementsByClassName("web-page")[0].innerHTML="Python"
-clone.getElementsByClassName("blade-style")[0].innerHTML="React"
-clone.getElementsByClassName("programming")[0].style.display="none"
-clone.classList.remove("border-left-green");
-//clone.getElementsByClassName("secundary")[0].innerHTML=""
-
-
-
-
-original.after(clone)
-
-
-
-
-
-
-
-
+createJobItem();
+createJobItem();
+createJobItem();
